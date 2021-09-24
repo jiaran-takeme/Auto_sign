3d512cf9d3db6b06ae7039c036aecd55d12bb8f0
jiaran-takeme <87650568+jiaran-takeme@users.noreply.github.com>
2021-09-24 08:36:36 +0800

    Add files via upload

--- 自动打卡.py
+++ 自动打卡.py
@@ -7,17 +7,17 @@ from email.utils import formataddr
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 
-now = time.localtime()  # 时间
+now = time.localtime() # 时间
 nowt = time.strftime("%Y-%m-%d %H:%M:%S", now)  # 设定时间格式
 
 def send_email():
     msg = MIMEText(result, 'html', 'utf-8')  # 正文
-    msg['From'] = formataddr(["自动打卡", "发送的邮箱"])  # 发信人
+    msg['From'] = formataddr(["自动打卡", "1668640479@qq.com"])  # 发信人
     msg['Subject'] = "自动打卡"  # 标题
 
-    server = smtplib.SMTP_SSL("服务器地址")
-    server.login("发送的邮箱", "授权码")
-    server.sendmail("发送的邮箱", "接收的邮箱", msg.as_string())
+    server = smtplib.SMTP_SSL("smtp.qq.com")
+    server.login("1668640479@qq.com", "zyeoipwopsbgghee")
+    server.sendmail("1668640479@qq.com", "zhiming20011020@sina.com", msg.as_string())
     server.quit()
 
 try:
@@ -25,8 +25,8 @@ try:
     option.add_argument('--headless')  # 设置无头启动
     browser = webdriver.Chrome(options=option)  # 调用无头的谷歌浏览器
 
-    # browser = webdriver.Chrome()  # 调用有窗口的浏览器
-    # browser.set_window_size(1920, 1080)
+    browser = webdriver.Chrome()  # 调用有窗口的浏览器
+    browser.set_window_size(1920, 1080)
 
     browser.implicitly_wait(10)  # 隐式等待
     browser.get("https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/first0")
@@ -37,13 +37,13 @@ try:
     except:
         pass
 
-    browser.find_element_by_name("uid").send_keys("学号")  # 学号
-    browser.find_element_by_name("upw").send_keys("密码\n")  # 密码
+    browser.find_element_by_name("uid").send_keys("202084170135")  # 学号
+    browser.find_element_by_name("upw").send_keys("Butanlianai789!\n")  # 密码
 
     time.sleep(1)
 
     browser.switch_to.frame('zzj_top_6s')
-    browser.find_element_by_xpath('/html/body/form/div/div[13]/div[5]/div[4]/span').click()
+    browser.find_element_by_xpath('/html/body/form/div/div[13]/div[3]/div[4]/span').click()
 
     select = Select(browser.find_element_by_name("myvs_13"))
     select.select_by_value("g")  # "g" 绿码,"r" 红码,"y" 黄码
